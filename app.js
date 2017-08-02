1cbe4761e05b277dcd8339b44f71f2195af8038d
Devanshu Awasthi <awasthi.devanshu@gmail.com>
2017-08-02 13:30:13 -0700

    threefiles

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ alert('That\'s right!');
 
 } else if( favColor === 'no' || favColor === 'n'){
     alert('Sorry, my favorite color is blue.');
-
+console.log('Is my favorite color blue?');
 }
 
 
@@ -23,13 +23,16 @@ if (favoriteFood === 'yes' || favoriteFood ==='y') {
     
 } else if ( favoriteFood ==='no'|| favoriteFood === 'n') {
     alert('Sorry, my favorite food is pizza.');
+console.log('Do I like pizza?');
 }
+
     var from = prompt('Was I born in Portland?').toLowerCase();
 
 if (from === 'yes' || from === 'y'){
     alert('That\'s correct!');
 } else if(from === 'no' || from === 'n') {
     alert('That is incorrect! I was born in Portland');
+    console.log('Was I born in Portland?');
 }
    var criminal = prompt('Do I like criminals?').toLowerCase();
    
@@ -37,6 +40,7 @@ if (from === 'yes' || from === 'y'){
        alert('Wrong! More like I don\'t appreciate them');
    } else if(criminal === 'no' || criminal === 'n') {
     alert(' Correct!');
+    console.log('Do I like criminals?');
    }
     
    var size = prompt('Do I have size 10 feet?').toLowerCase();
@@ -45,4 +49,5 @@ if (from === 'yes' || from === 'y'){
         alert('That\'s wrong!');
 } else if( size === 'no' || size === 'n') {
     alert ('Wrong, I wear Size 12s!');
+    console.log('Do I have size 10 feet?');
 }
